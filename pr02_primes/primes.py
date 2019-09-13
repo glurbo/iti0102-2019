14d2009aa11ac1c4643adb065e4215f492e14487
germo <glinde@ttu.ee>
2019-09-13 13:55:34 +0300

    ex01

--- pr02_primes/primes.py
+++ pr02_primes/primes.py
@@ -3,20 +3,20 @@
 
 def is_prime_number(number: int) -> bool:
     if number > 1:
-        for i in range(2, is_prime_number(number)):
+        if number == 2:
+            return True
+        for i in range(2, number):
             if (number % i) == 0:
                 return False
             else:
                 return True
-    return None
+        return
 
 
-if __name__ == '__is_prime_number__':
+if __name__ == '__main__':
     print(is_prime_number(2))
     print(is_prime_number(89))
     print(is_prime_number(23))
     print(is_prime_number(4))
     print(is_prime_number(7))
     print(is_prime_number(88))
-
-"""true true true false true false"""
