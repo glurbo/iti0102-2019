69196c541476b39bb6cce13812c6ddcd14a42631
germo <glinde@ttu.ee>
2019-09-13 14:03:23 +0300

    pr02_primes

--- pr02_primes/primes.py
+++ pr02_primes/primes.py
@@ -3,14 +3,12 @@
 
 def is_prime_number(number: int) -> bool:
     """
-      Check if number (given in function parameter) is prime.
-
-      If number is prime -> return True
-      If number is not prime -> return False
-
-      :param number: number for check.
-      :return: boolean True if number is prime or False if number is not prime.
-      """
+    Check if number (given in function parameter) is prime.
+    If number is prime -> return True
+    If number is not prime -> return False
+    :param number: number for check.
+    :return: boolean True if number is prime or False if number is not prime.
+    """
     if number > 1:
         if number == 2:
             return True
