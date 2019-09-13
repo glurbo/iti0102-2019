2e2586b7b2a116e357b7f4b4298427c7e6b9eed2
germo <glinde@ttu.ee>
2019-09-13 16:30:11 +0300

    pr02_primes

--- pr02_primes/primes.py
+++ pr02_primes/primes.py
@@ -12,16 +12,13 @@ def is_prime_number(number: int) -> bool:
     :return: boolean True if number is prime or False if number is not prime.
     """
     if number > 1:
-        if number == 2:
-            return True
         for i in range(2, number):
             if (number % i) == 0:
                 return False
-            else:
-                return True
+        else:
+            return True
     else:
         return False
-    return None
 
 
 if __name__ == '__main__':
