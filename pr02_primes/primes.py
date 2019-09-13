26be564037d23c319cf659f5e7fa3271bf3fa540
germo <glinde@ttu.ee>
2019-09-13 13:28:46 +0300

    ex01

---
+++ pr02_primes/primes.py
@@ -0,0 +1,19 @@
+"""Primes identifier."""
+
+
+def is_prime_number(number: int) -> bool:
+    if number > 1:
+        for i in range(2, is_prime_number(number)):
+            if (number % i) == 0:
+                return False
+            else:
+                return True
+        return None
+    print(is_prime_number(2))
+    print(is_prime_number(89))
+    print(is_prime_number(23))
+    print(is_prime_number(4))
+    print(is_prime_number(7))
+    print(is_prime_number(88))
+
+"""true true true false true false"""
