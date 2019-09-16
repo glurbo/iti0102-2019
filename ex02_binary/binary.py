1c7dfde3301ff3fbdfdb8d9c31537402aea4aacb
Germo Linder <glinde@ttu.ee>
2019-09-16 13:00:26 +0300

    ex02_binary

--- ex02_binary/binary.py
+++ ex02_binary/binary.py
@@ -10,11 +10,10 @@ def dec_to_binary(decimal: int) -> str:
     """
     if decimal == 0:
         return str(decimal)
-    elif decimal > 0:
-        dec_to_binary(decimal // 2)
-        binary = decimal % 2
-        print(binary, end='')
-    return ''
+    elif decimal >= 1:
+        binary = decimal // 2
+        print(binary % 2, end='')
+        return ''
 
 
 def binary_to_dec(binary: str) -> int:
@@ -41,3 +40,25 @@ if __name__ == "__main__":
     print(binary_to_dec("1111"))  # -> 15
     print(binary_to_dec("10101"))  # -> 21
     print(binary_to_dec("10010"))  # -> 18
+
+
+import csv
+# import csv is REQUIRED to read in csv file
+
+
+
+with open('C:\\Projects\\kool\\decimal_binary_numbers\\numbers.csv', 'r') as f:
+    reader = csv.reader(f)
+    decimal_binary_list = list(reader)
+
+    print("DECIMAL TO BINARY")
+    for numbers in decimal_binary_list:
+        # decimal x in binary is y: True/False
+        print(f"decimal {numbers.__getitem__(0)} in binary is {numbers.__getitem__(1)}: "
+              f"{dec_to_binary(int(numbers.__getitem__(0))) == numbers.__getitem__(1)}")
+
+    print("BINARY TO DECIMAL")
+    for numbers in decimal_binary_list:
+        # binary x in decimal is x: True/False
+        print(f"binary {numbers.__getitem__(1)} in decimal is {numbers.__getitem__(0)}: "
+              f"{binary_to_dec(numbers.__getitem__(1)) == int(numbers.__getitem__(0))}")
