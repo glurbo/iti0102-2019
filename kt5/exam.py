8192601760a1f8bf83c94d7566d26c1a7eb47b45
glinde <glinde@ttu.ee>
2019-11-15 11:37:50 +0200

    kt5 exam

--- kt5/exam.py
+++ kt5/exam.py
@@ -108,7 +108,7 @@ if __name__ == '__main__':
     print(sum_elements_around_last_three([1, 3, 7]))  # -> 8
     print(sum_elements_around_last_three([1, 2, 3, 4, 6, 4, 3, 4, 5, 3, 4, 5, 6]))  # -> 9
     print(sum_elements_around_last_three([1, 2, 3, 4, 6, 4, 3, 4, 5, 3, 3, 2, 3]))  # -> 5
-    print(sum_elements_around_last_three([1, 2, 3])) # -> 0
+    print(sum_elements_around_last_three([1, 2, 3]))  # -> 0
     print("------------------------------------------------")
     print(pentabonacci(5))  # -> 1
     print(pentabonacci(10))  # -> 3
