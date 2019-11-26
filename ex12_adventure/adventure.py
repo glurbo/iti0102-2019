ee29a0f1711aa5315e1206732e59437a8b23a880
germo <glinde@ttu.ee>
2019-11-26 21:41:34 +0200

    ex12_adventure

--- ex12_adventure/adventure.py
+++ ex12_adventure/adventure.py
@@ -148,16 +148,15 @@ class World:
         for c in self.adventurerlist:
             if c.name == name:
                 self.active_adventurers.append(c)
-        for c in range(len(self.active_adventurers)):
-            self.adventurerlist.remove(self.active_adventurers[c])
+                self.adventurerlist.remove(c)
 
     def add_all_of_class_type(self, class_type):
         """Add all adventurers of a certain class type to active adventurer list."""
         for c in self.adventurerlist:
             if c.class_type == class_type:
                 self.active_adventurers.append(c)
-        for c in range(len(self.active_adventurers)):
-            self.adventurerlist.remove(self.active_adventurers[c])
+        for c in self.active_adventurers:
+            self.adventurerlist.remove(c)
 
     def add_all(self):
         """Add all adventurers to active adventurer list."""
@@ -172,10 +171,10 @@ class World:
 
     def add_monster_by_name(self, name):
         """Add a monster to active monster list by name."""
-        for c in range(len(self.monsterlist)):
-            if self.monsterlist[c].name == name:
+        for c in self.monsterlist:
+            if c.name == name:
                 self.active_monsters.append(c)
-                self.monsterlist.remove(self.monsterlist[c])
+                self.monsterlist.remove(c)
 
     def add_strongest_monster(self):
         """Add the monster with highest power level to active monster list."""
@@ -194,8 +193,8 @@ class World:
         for c in self.monsterlist:
             if c.mon_type == mon_type:
                 self.active_monsters.append(c)
-        for c in range(len(self.active_monsters)):
-            self.monsterlist.remove(self.active_monsters[c])
+        for c in self.active_monsters:
+            self.monsterlist.remove(c)
 
     def add_all_monsters(self):
         """Add all monsters to active monster list."""
